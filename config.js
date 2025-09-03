91502871840b5e2014b08d73b86091100ebe1a23
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-03 10:07:03 -0700

    Update database user credentials for security compliance.

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     dbConfig: {
         // Cấu hình database, sử dụng cho việc lưu trữ thông tin
         host: 'localhost',
-        user: 'username',
-        password: 'password',
+        user: 'new_username',
+        password: 'new_password',
         database: 'he_thong_phan_phoi_thong_tin'
     }
 };
