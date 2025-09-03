a073657a1c9370747919eb5d7c3c362931457bdb
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-03 10:06:13 -0700

    Handle user not found case in getUserInfo function

--- controller.js
+++ controller.js
@@ -4,6 +4,9 @@ exports.getUserInfo = async (req, res) => {
     try {
         const userId = req.params.id;
         const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
         res.json(user);
     } catch (error) {
         res.status(500).send(error);
