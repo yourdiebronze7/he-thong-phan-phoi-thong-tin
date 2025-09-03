61ca841b10baf3986de114e3773c3f6c94e4c6c5
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-03 10:05:34 -0700

    feat: initial commit for config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dbConfig: {
+        // Cấu hình database, sử dụng cho việc lưu trữ thông tin
+        host: 'localhost',
+        user: 'username',
+        password: 'password',
+        database: 'he_thong_phan_phoi_thong_tin'
+    }
+};
